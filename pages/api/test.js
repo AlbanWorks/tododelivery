ccd736d6292fe1268824a102c41b5099741cac1b
Mi Nombre <lizardosantiagomail@gmail.com>
2022-01-07 00:53:56 -0300

    guayando4

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,26 +1,27 @@
 import {firestore} from '../../FirebaseAdminConfig'
 import {db} from '../../firebaseConfig'
+import { collection, get, doc, getDoc, setDoc, updateDoc } from "firebase-admin/firestore"
 
 
 export default async function handler(req, res) {
     
-    const getProduct = async (category, ID) => {
-        const docRef = doc(firestore, category, ID);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
+    const getProduct = async () => {
+        const docRef = firestore.doc('frutas/anana');
+        const docSnap = await docRef.get();
+        if (docSnap.exists) {
             return docSnap.data();
         }     
         else {
         // doc.data() will be undefined in this case
-         return false
+         return "no existe"
         }
     }
 
     if(req.method ==='POST'){
        //const datos = await getProduct("frutas","anana") 
       // console.log(datos)
-       console.log(firestore)
-        res.status(200).json(firestore)
+        const deira = await getProduct()
+        res.status(200).json(deira)
     }
 
    
