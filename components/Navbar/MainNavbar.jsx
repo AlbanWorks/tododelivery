70c04dfbc3aa671c08700c96e642a81a3a0d3f41
Mi Nombre <lizardosantiagomail@gmail.com>
2022-01-09 16:32:44 -0300

    mejoras en el frontend1

--- components/Navbar/MainNavbar.jsx
+++ components/Navbar/MainNavbar.jsx
@@ -9,7 +9,7 @@ const MainNavbar = () => {
             </nav>
             <div className={classes.Wave} >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="rgb(54, 98, 219)" fill-opacity="1" d="M0,224L80,234.7C160,245,320,267,480,234.7C640
+                    <path fill="rgb(54, 98, 219)" fill0pacity="1" d="M0,224L80,234.7C160,245,320,267,480,234.7C640
                     ,203,800,117,960,106.7C1120,96,1280,160,1360,192L1440,224L1440,0L1360,0C1280,0,1120,0,
                     960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z">
                     </path>
